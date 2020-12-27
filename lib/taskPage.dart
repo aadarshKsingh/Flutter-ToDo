2e1b9f3e703c397df02a1d17e9f0ac0e9f986a52
realityislie <aadarshkumarsingh198@gmail.com>
2020-12-27 17:25:41 +0530

    Fetch data from external json

--- lib/taskPage.dart
+++ lib/taskPage.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/foundation.dart';
 import 'tasksList.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 class taskPage extends StatefulWidget {
   @override
@@ -12,122 +9,123 @@ class taskPage extends StatefulWidget {
 }
 
 class _taskPageState extends State<taskPage> {
-  @override
-  void initState() {
-    checkPermission();
-    super.initState();
+  Future getData() async {
+    await Provider.of<tasksList>(context, listen: false).readContent();
   }
 
-  Future<PermissionStatus> checkPermission() async {
-    if (await Permission.storage.isDenied == true ||
-        await Permission.storage.isUndetermined) {
-      await Permission.storage.request();
-      if (await Permission.storage.isGranted == false) {
-        Fluttertoast.showToast(msg: "Request for storage denied");
-        SystemNavigator.pop();
-      }
-    }
+  @override
+  void initState() {
+    // TODO: implement initState
+    getData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Consumer<tasksList>(builder: (context, value, child) {
-              if (value.getTaskList.isEmpty)
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [Text(' ¯\_(ツ)_/¯'), Text("No Tasks added.")],
-                  ),
-                );
-              else
-                return ListView.builder(
-                    itemCount: value.getTaskList.length,
-                    itemBuilder: (context, index) {
-                      return Dismissible(
-                        dismissThresholds: {
-                          DismissDirection.startToEnd: 0.7,
-                          DismissDirection.endToStart: 0.7
-                        },
-                        background: Container(
-                          color: Colors.redAccent,
-                          child: Padding(
-                            child: Text(
-                              "Remove Task",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 17),
-                            ),
-                            padding: EdgeInsets.fromLTRB(30, 20, 260, 0),
+      body: Column(children: [
+        Expanded(
+          child: Consumer<tasksList>(builder: (context, value, child) {
+            if (value.getTaskList.length == 0)
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '  ( ╯° - ° ) ╯',
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                    ),
+                    Text(
+                      "You are free as a bird",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+                    )
+                  ],
+                ),
+              );
+            else
+              return ListView.builder(
+                  itemCount: value.getTaskList.length,
+                  itemBuilder: (context, index) {
+                    return Dismissible(
+                      dismissThresholds: {
+                        DismissDirection.startToEnd: 0.7,
+                        DismissDirection.endToStart: 0.7
+                      },
+                      background: Container(
+                        color: Colors.redAccent,
+                        child: Padding(
+                          child: Text(
+                            "Remove Task",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 17),
+                          ),
+                          padding: EdgeInsets.fromLTRB(30, 20, 260, 0),
+                        ),
+                      ),
+                      secondaryBackground: Container(
+                        color: Colors.redAccent,
+                        child: Padding(
+                          padding: EdgeInsets.fromLTRB(280, 20, 0, 0),
+                          child: Text(
+                            "Remove Task",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 17),
                           ),
                         ),
-                        secondaryBackground: Container(
-                          color: Colors.redAccent,
-                          child: Padding(
-                            padding: EdgeInsets.fromLTRB(280, 20, 0, 0),
-                            child: Text("Remove Task",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 17)),
-                          ),
-                        ),
-                        key: ValueKey(
-                            Provider.of<tasksList>(context, listen: false)
-                                .getTaskList[index]),
-                        onDismissed: (direction) {
-                          Provider.of<tasksList>(context, listen: false)
-                              .removetaskValue(index, direction);
-                        },
-                        child: Card(
-                            color: Colors.redAccent.shade50,
-                            margin: EdgeInsets.symmetric(
-                                horizontal: 5, vertical: 3),
-                            elevation: 3,
-                            child: value.getTaskDescList[index].isEmpty
-                                ? ListTile(
-                                    leading: Padding(
+                      ),
+                      key: ValueKey(value.getTaskList),
+                      onDismissed: (direction) {
+                        Provider.of<tasksList>(context, listen: false)
+                            .removeTaskValue(index);
+                      },
+                      child: Card(
+                          color: Colors.redAccent.shade50,
+                          margin:
+                              EdgeInsets.symmetric(horizontal: 5, vertical: 3),
+                          elevation: 3,
+                          child: value.getTaskDescList[index].isEmpty
+                              ? ListTile(
+                                  leading: Padding(
+                                    padding: EdgeInsets.fromLTRB(10, 3, 0, 0),
+                                    child: Text(
+                                      (index + 1).toString(),
+                                    ),
+                                  ),
+                                  trailing: Text(
+                                    value.getDateTime[index],
+                                    style: TextStyle(fontSize: 13),
+                                  ),
+                                  title: Text(value.getTaskList[index]),
+                                )
+                              : ExpansionTile(
+                                  leading: Padding(
                                       padding: EdgeInsets.fromLTRB(10, 3, 0, 0),
-                                      child: Text(
-                                        (index + 1).toString(),
-                                      ),
-                                    ),
-                                    trailing: Text(
-                                      value.getDateTime[index],
-                                      style: TextStyle(fontSize: 13),
-                                    ),
-                                    title: Text(value.getTaskList[index]),
-                                  )
-                                : ExpansionTile(
-                                    leading: Padding(
-                                        padding:
-                                            EdgeInsets.fromLTRB(10, 3, 0, 0),
-                                        child: Text((index + 1).toString())),
-                                    title: Text(value.getTaskList[index]),
-                                    trailing: Text(
-                                      value.getDateTime[index],
-                                      style: TextStyle(fontSize: 13),
-                                    ),
-                                    children: [
-                                      Container(
-                                        height: 40,
-                                        width: double.infinity,
-                                        child: Padding(
-                                          padding: EdgeInsets.only(left: 72),
-                                          child: Text(
-                                            value.getTaskDescList[index],
-                                            textAlign: TextAlign.start,
-                                          ),
+                                      child: Text((index + 1).toString())),
+                                  title: Text(value.getTaskList[index]),
+                                  trailing: Text(
+                                    value.getDateTime[index],
+                                    style: TextStyle(fontSize: 13),
+                                  ),
+                                  children: [
+                                    Container(
+                                      height: 40,
+                                      width: double.infinity,
+                                      child: Padding(
+                                        padding: EdgeInsets.only(left: 72),
+                                        child: Text(
+                                          value.getTaskDescList[index],
+                                          textAlign: TextAlign.start,
                                         ),
                                       ),
-                                    ],
-                                  )),
-                      );
-                    });
-            }),
-          ),
-        ],
-      ),
+                                    ),
+                                  ],
+                                )),
+                    );
+                  });
+          }),
+        ),
+      ]),
     );
   }
 }
--- lib/tasksList.dart
+++ lib/tasksList.dart
@@ -1,12 +1,46 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
-import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 class tasksList extends ChangeNotifier {
   List<String> _taskList = [];
   List<String> _taskDescList = [];
   List<String> _taskDateTime = [];
+
+  var taskJSON = new Map<String, dynamic>();
+  addtaskValue(String title, String subtitle) {
+    _taskList.add(title);
+    _taskDescList.add(subtitle);
+    _taskDateTime.add(getCurrentDateTime());
+    taskJSON['title'] = _taskList;
+    taskJSON['description'] = _taskDescList;
+    taskJSON['dateTime'] = _taskDateTime;
+    writeContent();
+    notifyListeners();
+  }
+
+  removeTaskValue(int index) {
+    _taskList.removeAt(index);
+    _taskDescList.removeAt(index);
+    _taskDateTime.removeAt(index);
+    clearJSON();
+    taskJSON['title'] = _taskList;
+    taskJSON['description'] = _taskDescList;
+    taskJSON['dateTime'] = _taskDateTime;
+    notifyListeners();
+  }
+
+  removeall() async {
+    _taskList = [];
+    _taskDescList = [];
+    _taskDateTime = [];
+    await clearJSON();
+    notifyListeners();
+  }
 
   getCurrentDateTime() {
     var currentDateTime = DateTime.now();
@@ -15,26 +49,41 @@ class tasksList extends ChangeNotifier {
     return formattedDateTime;
   }
 
-  addtaskValue(String title, String subtitle) {
-    _taskList.add(title);
-    _taskDescList.add(subtitle);
-    _taskDateTime.add(getCurrentDateTime());
-    notifyListeners();
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    return directory.path;
   }
 
-  removetaskValue(int index, direction) {
-    _taskList.removeAt(index);
-    _taskDescList.removeAt(index);
-    _taskDateTime.removeAt(index);
-    Fluttertoast.showToast(msg: "Task Removed", gravity: ToastGravity.BOTTOM);
-    notifyListeners();
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/tasks.json');
   }
 
-  removeall() {
-    _taskList = [];
-    _taskDescList = [];
-    _taskDescList = [];
-    notifyListeners();
+  Future<void> readContent() async {
+    try {
+      File file = await _localFile;
+      String jsonString = await file.readAsString();
+      var data = jsonDecode(jsonString);
+      for (int i = 0; i < data['title'].length; i++) {
+        _taskList.add(data["title"][i]);
+        _taskDescList.add(data["description"][i]);
+        _taskDateTime.add(data["dateTime"][i]);
+      }
+      notifyListeners();
+    } catch (FileSystemException) {
+      print("tasks.json not found");
+    }
+  }
+
+  Future<File> writeContent() async {
+    final path = await _localFile;
+    String serializedMap = jsonEncode(taskJSON);
+    return path.writeAsString(serializedMap);
+  }
+
+  clearJSON() async {
+    final path = await _localFile;
+    return path.writeAsString("");
   }
 
   List<String> get getTaskList => _taskList;
